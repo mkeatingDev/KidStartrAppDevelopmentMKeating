1dd2f9ca8a85932a4a655f1d85feeaf76b20b2d0
Matt Keating <mattkeating@matts-macbook-pro.local>
2016-06-13 14:29:53 -0400

    Initial Commit

---
+++ KidStartr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KidStartr
+//
+//  Created by Matt Keating on 6/13/16.
+//  Copyright © 2016 Matt Keating. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
